10bdc7790d8233f6d250711514438858c99ddb70
anikrahman59 <anik.rahman578@gmail.com>
2022-06-20 00:42:22 +0600

    client migration

---
+++ database/migrations/2022_06_19_183926_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('confirm_password');
+            $table->string('gender');
+            $table->string('dob');
+            $table->string('blood_group');
+            $table->string('phone');
+            $table->string('address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
