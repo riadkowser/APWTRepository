ee7e76d84f4fd358b0d62ac0482749272fec0941
riadkowser <riadkowser@gmail.com>
2022-07-29 11:25:01 +0600

    React App

--- APWT TASK_7 React API Data/src/App.js
+++ APWT TASK_7 React API Data/src/App.js
@@ -9,14 +9,8 @@ function App() {
         <p>
           Riad Kowser
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+          
       </header>
     </div>
   );
