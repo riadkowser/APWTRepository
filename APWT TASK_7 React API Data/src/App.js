45845bb7569d2bdda4a136f41e0d81fa3f5153da
riadkowser <riadkowser@gmail.com>
2022-07-29 11:13:10 +0600

    React App

--- APWT TASK_7 React API Data/src/App.js
+++ APWT TASK_7 React API Data/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Riad KOwser
+          Riad Kowser
         </p>
         <a
           className="App-link"
